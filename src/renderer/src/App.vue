a29d5fdc48a66140e92769dc44bc9aa174d32dfc
qingw1230 <1366502108@qq.com>
2025-01-13 19:30:20 +0800

    初始化项目

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Versions from './components/Versions.vue'
+</script>
+
+<template>
+  <div>我的第一个应用程序</div>
+</template>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
